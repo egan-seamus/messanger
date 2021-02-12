5bb2ab105fac376a64bc995fb2fafd9ee33fa3d9
Seamus Egan <egan.seam@husky.neu.edu>
2021-02-11 22:35:09 -0500

    added password hashing & checking

--- messaging/views.py
+++ messaging/views.py
@@ -9,18 +9,22 @@ from django.http import HttpResponseServerError
 from django.http import JsonResponse
 from json import loads
 from .models import CustomUser
+from django.contrib.auth import hashers
 
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
        mJson = loads(request.body.decode("utf-8"))
        username = mJson.get('username')
+       existing_uname_list = CustomUser.objects.filter(username=username)
+       if len(existing_uname_list) < 1:
+           return HttpResponseBadRequest("No such user exists")
        password = mJson.get('password')
-       print(username)
-       print (password)
-       return JsonResponse({
-           "response" : "post successful"
-       })
+       if not hashers.check_password(password, existing_uname_list[0].password):
+           return HttpResponseBadRequest("incorrect password")
+       else:
+            return HttpResponse("Logged you in")
+
     else:
         return HttpResponseBadRequest("Missing username or password")
 
@@ -33,7 +37,8 @@ def register(request):
        if(len(conflicts) > 0):
            return HttpResponseServerError("Username already taken")
        password = mJson.get('password')
-       new_user = CustomUser(username=username, password=password)
+       hashed_password = hashers.make_password(password)
+       new_user = CustomUser(username=username, password=hashed_password)
        new_user.save()
        return HttpResponse("User Created")
 
